62cc4f88b9e9fbff324941fa4b6001d3373f6a52
adrianz261 <adrian.zuchowski@outlook.com>
2021-06-14 19:15:50 +0100

    Added constructors for Ship class.

---
+++ Ship.cpp
@@ -0,0 +1,15 @@
+#include "Ship.hpp"
+
+Ship::Ship() = default;
+
+Ship::Ship(int id, const std::string& name, unsigned int speed, unsigned int maxCrew, unsigned int capacity)
+    : id_(id), name_(name), speed_(speed), maxCrew_(maxCrew), capacity_(capacity)
+{
+
+}
+
+Ship::Ship(int id, unsigned int speed, unsigned int maxCrew)
+    : Ship(id, "", speed, maxCrew, 1)
+{
+
+}
--- Ship.hpp
+++ Ship.hpp
@@ -5,9 +5,14 @@
 
 class Ship
 {
-    int id = -1;
-    std::string name_;
-    unsigned int speed_;
-    unsigned int maxCrew_;
-    unsigned int capacity_;
+    int id_ = -1;
+    std::string name_ = "";
+    unsigned int speed_ = 0;
+    unsigned int maxCrew_ = 0;
+    unsigned int capacity_ = 0;
+
+public:
+    Ship::Ship();
+    Ship::Ship(int id, unsigned int speed, unsigned int maxCrew);
+    Ship::Ship(int id, const std::string& name, unsigned int speed, unsigned int maxCrew, unsigned int capacity);
 };
