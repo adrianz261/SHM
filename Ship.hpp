dd9cf6f18894b71343df879eeabe446191bc4470
adrianz261 <adrian.zuchowski@outlook.com>
2021-06-14 19:01:53 +0100

    Initial commit.

---
+++ Ship.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class Ship
+{
+    int id = -1;
+    std::string name_;
+    unsigned int speed_;
+    unsigned int maxCrew_;
+    unsigned int capacity_;
+};
